e11b5744fe0e0a6dbc5326bc6e1b756a4f7ef7c9
Balaji J <balajijanarthanan1997@gmail.com>
2021-07-06 10:24:45 +0530

    Adding Cart Items: using Cart context and Reducer. working refs and forward Refs.

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,15 +1,45 @@
-import React from "react";
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+const cartReducer = (state, action) => {
+  if (action.type === "ADD_CART_ITEM") {
+    const updatedItems = state.items.concat(action.payload);
+    const updatedTotalAmount =
+      state.items.totalAmount + action.payload.price * action.payload.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  } else if (action.type === "REMOVE_CART_ITEM") {
+  }
+  return defaultCartState;
+};
+
 const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
-  const removeItemFromCartHandler = (id) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD_CART_ITEM", payload: item });
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE_CART_ITEM", payload: id });
+  };
+
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
+
   return (
     <CartContext.Provider value={cartContext}>
       {props.children}
